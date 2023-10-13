d3a69eb12d9b8b5d47884f65c06f8e6ea0d472da
Petr Musil <petr0musil@gmail.com>
2023-10-13 11:24:16 +0200

    Fourth question - query for salaries change added.

--- 4_fourth_question.sql
+++ 4_fourth_question.sql
@@ -10,3 +10,16 @@ WHERE
 	tpm.industry_name = 'Vzdělávání'
 GROUP BY 
 	tpm.year_cp;
+
+-- změny ceny mezd za všechna odvětví
+SELECT
+	tpm.year_cp,
+	sum(tpm.average_salary_CZK) AS avg_salary_box_current,
+	lead(sum(tpm.average_salary_CZK)) OVER (ORDER BY tpm.year_cp) AS avg_price_box_next,
+	round(100 * (lead(sum(tpm.average_salary_CZK)) OVER (ORDER BY tpm.year_cp) / sum(tpm.average_salary_CZK)) - 100, 2) AS annual__change_box
+FROM
+	t_petr_musil_project_SQL_primary_final tpm
+WHERE
+	tpm.food_category = 'Banány žluté'
+GROUP BY 
+	tpm.year_cp;
