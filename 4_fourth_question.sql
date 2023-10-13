52004b09d4cf4dbb1d47ca551557fb6909bd4206
Petr Musil <petr0musil@gmail.com>
2023-10-13 11:05:13 +0200

    Fourth question - format of sql correction.

--- 4_fourth_question.sql
+++ 4_fourth_question.sql
@@ -2,8 +2,8 @@
 SELECT
 	tpm.year_cp,
 	sum(average_prices) AS avg_price_box_current,
-	lead(sum(average_prices)) over (ORDER by tpm.year_cp) AS avg_price_box_next,
-	round(100 * (lead(sum(average_prices)) over (ORDER by tpm.year_cp) / sum(average_prices)) - 100, 2) AS annual__change_box
+	lead(sum(average_prices)) OVER (ORDER by tpm.year_cp) AS avg_price_box_next,
+	round(100 * (lead(sum(average_prices)) OVER (ORDER by tpm.year_cp) / sum(average_prices)) - 100, 2) AS annual__change_box
 FROM
 	t_petr_musil_project_SQL_primary_final tpm
 WHERE
