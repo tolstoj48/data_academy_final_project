b380966da812905a00ffd363f1002702b47ac9c7
Petr Musil <petr0musil@gmail.com>
2023-10-17 09:09:37 +0200

    Corrected names of columns in SELECT.

--- 3_third_question.sql
+++ 3_third_question.sql
@@ -3,8 +3,8 @@ SELECT
 	tpm.food_category,
 	tpm.average_prices AS avg_prices_current_year,
 	lead(tpm.average_prices) OVER(PARTITION BY tpm.food_category ORDER BY tpm.year_cp) AS avg_prices_next_year,
-	round(((lead(tpm.average_prices) OVER(PARTITION BY tpm.food_category ORDER BY tpm.year_cp) / tpm.average_prices) - 1) * 100, 2) AS annual_change_in_price,
-	round(((last_value(tpm.average_prices) OVER(PARTITION BY tpm.food_category ORDER BY tpm.year_cp ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) / first_value(tpm.average_prices) OVER(PARTITION BY tpm.food_category ORDER BY tpm.year_cp ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)) - 1) * 100, 2)AS overall_change_price
+	round(((lead(tpm.average_prices) OVER(PARTITION BY tpm.food_category ORDER BY tpm.year_cp) / tpm.average_prices) - 1) * 100, 2) AS annual_change_in_price_perc,
+	round(((last_value(tpm.average_prices) OVER(PARTITION BY tpm.food_category ORDER BY tpm.year_cp ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) / first_value(tpm.average_prices) OVER(PARTITION BY tpm.food_category ORDER BY tpm.year_cp ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)) - 1) * 100, 2)AS overall_change_price_perc
 FROM
 	t_petr_musil_project_SQL_primary_final tpm
 WHERE
